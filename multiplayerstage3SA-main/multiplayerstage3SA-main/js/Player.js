681808fe87f3e54b7cd69b7fdb25a9ead522668d
yuvaraju5220 <93331364+yuvaraju5220@users.noreply.github.com>
2022-02-18 20:10:36 +0530

    Add files via upload

---
+++ multiplayerstage3SA-main/multiplayerstage3SA-main/js/Player.js
@@ -0,0 +1,68 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+   addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+    });
+  }
+  //TA
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+ 
+  //Bp
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  //Bp
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }  
+     update(){
+    var playerindex="players/player"+ this.index
+    database.ref(playerindex).update({
+      positionX:this.positionX,
+      positionY:this.positionY
+    })
+
+
+     }
+ 
+      static getPlayersInfo(){
+     var playerinforef=database.ref("players")
+     playerinforef.on("value",data => {
+       allPlayers=data.val()
+     })
+
+      }
+
+
+
+}
